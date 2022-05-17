53f8bae46e4ad0f177fd4be09e51b7c153d53156
herkusas <herkus.gaidanis@gmail.com>
2022-05-17 17:50:20 +0300

    final

--- local/db/migrations/V1__initial.sql
+++ local/db/migrations/V1__initial.sql
@@ -89,31 +89,6 @@ CREATE TABLE IF NOT EXISTS api_resource_scopes
 
 INSERT INTO clients(
     client_id, client_name, access_token_lifetime)
-VALUES ('test_client', 'test client', 3600);
-
-INSERT INTO api_scopes(
-    name)
-VALUES ('urn:masters:secure_api:weather:get');
-
-INSERT INTO client_secrets(
-    client_id, value, type)
-VALUES (1, '42BD490AC869ED79253392E00E31E0CCCEEF0724', 'X509Thumbprint');
-
-INSERT INTO client_scopes(
-    client_id, scope_id)
-VALUES (1, 1);
-
-INSERT INTO api_resources(
-    name)
-VALUES ('https://localhost:7038');
-
-INSERT INTO api_resource_scopes(
-    api_resource_id, scope_id)
-VALUES (1, 1);
-
-
-INSERT INTO clients(
-    client_id, client_name, access_token_lifetime)
 VALUES ('admin', 'Admin', 300);
 
 INSERT INTO api_scopes(
@@ -126,15 +101,15 @@ VALUES ('urn:masters:admin_api:resources:manage');
 
 INSERT INTO client_secrets(
     client_id, value, type)
-VALUES (2, 'jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=', 'SharedSecret');
+VALUES (1, 'jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=', 'SharedSecret');
 
 INSERT INTO client_scopes(
     client_id, scope_id)
-VALUES (2, 2);
+VALUES (1, 1);
 
 INSERT INTO client_scopes(
     client_id, scope_id)
-VALUES (2, 3);
+VALUES (1, 2);
 
 INSERT INTO api_resources(
     name)
@@ -142,8 +117,8 @@ VALUES ('https://localhost:7092');
 
 INSERT INTO api_resource_scopes(
     api_resource_id, scope_id)
-VALUES (2, 2);
+VALUES (1, 1);
 
 INSERT INTO api_resource_scopes(
     api_resource_id, scope_id)
-VALUES (2, 3);
+VALUES (1, 2);
